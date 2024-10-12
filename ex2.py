4a6d9a14f4c52b96e3dca05f05ee4d601d9c2212
sl-kchr <kucherslavka@gmail.com>
2024-10-12 21:02:30 +0200

    6

--- ex2.py
+++ ex2.py
@@ -1,9 +1,9 @@
 from pathlib import Path
+import re
 p = Path('HW.txt')
 p.write_text('60b90c1c13067a15887e1ae1,Tayson,3\n60b90c2413067a15887e1ae2,Vika,1\n60b90c2e13067a15887e1ae3,Barsik,2\n60b90c3b13067a15887e1ae4,Simon,12\n60b90c4613067a15887e1ae5,Tessi,5')
 
 def get_cats_info(path):
-    cats_info = {}
     cats_data = []
     path = Path(path)
 
@@ -14,7 +14,7 @@ def get_cats_info(path):
                 lines = path.splitlines()
                 for i in lines:
                     i = i.split(',')
-                    cats_info.update({"id":(i[1]), "name":(i[1]), "age": (i[2])})
+                    cats_info = {"id":(i[0]), "name":(i[1]), "age": (i[2])}
                     cats_data.append(cats_info)
                 return cats_data
             else:
@@ -23,4 +23,4 @@ def get_cats_info(path):
             print('На жаль, файл не був знайдений')
     except FileNotFoundError:
         print('На жаль, файл не був знайдений')
-get_cats_info('HW.txt')
+print(get_cats_info('HW.txt'))
